4a99a7c5ebf0559cb17e9e652422cda0a6a0f11b
lielorenstein <lielorenstein10@gmail.com>
2019-12-10 21:23:19 +0200

    updeted

--- DAL/IDAL.cs
+++ DAL/IDAL.cs
@@ -1,5 +1,8 @@
 ﻿using BE;
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +13,11 @@ namespace DAL
 {
     public interface IDAL
     {
+<<<<<<< Updated upstream
         void addRequst(GuestRequest newRequest);
+=======
+        void addRequest(GuestRequest newRequest);
+>>>>>>> Stashed changes
         void updateRequest(GuestRequest update);
         void addUnit(HostingUnit newUnit);
         void deleteUnit(HostingUnit delUnit);
@@ -18,6 +25,7 @@ namespace DAL
         void addOrder(Order newOrder);
         void updateOrder(Order update);
         void unitList(List<HostingUnit> unitsList);
+<<<<<<< Updated upstream
         void customerList(List<GuestRequest> customersList);
         void orderList(List<Order> ordersList);
         List <string> bankList(List <BankAccount> bankLists);
@@ -25,5 +33,10 @@ namespace DAL
 
 
 
+=======
+        void custumerList(List<GuestRequest> custumersList);
+        void orderList(List<Order> ordersList);
+        List<string> bankList( List<BankAccount> bankLists) ;
+>>>>>>> Stashed changes
     }
 }
