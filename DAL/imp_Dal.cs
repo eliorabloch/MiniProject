42ad0b5632ba734f75148b5c03c640b1a3220712
lielorenstein <lielorenstein10@gmail.com>
2020-01-14 12:07:29 +0200

    ui manager page

--- DAL/imp_Dal.cs
+++ DAL/imp_Dal.cs
@@ -78,20 +78,20 @@ namespace DAL
         public List<Host> GetHostList()
         {
             return (from h in DataSource.unitList
-                            select (Host)h.Owner.Clone()).ToList();
+                    select (Host)h.Owner.Clone()).ToList();
             //List<Host> myhostlist = (from h in DataSource.unitList
             //                         select (Host)h.Owner.Clone()).ToList();
             //List<Host> myhostlist2 = new List<Host>();
             //foreach (var host in myhostlist)
             //{
-            //    foreach (var host2 in myhostlist2)
+            //    myhostlist2.Add(host);
+            //    foreach (var host2 in myhostlist)
             //    {
             //        if (host.HostId != host2.HostId)
             //        {
             //            myhostlist2.Add(host2);
             //        }
             //    }
-            //    //  return myhostlist2;
             //}
             //return myhostlist2;
         }
